cb28834e972976de02af07aa4be7cd1c11ad8901
Dang Zhiqiang <dzq008@163.com>
2014-11-23 23:12:27 +0800

    change start_test.sh format and add PGFAPI_LOGDIR

--- start_test.sh
+++ start_test.sh
@@ -16,8 +16,6 @@ export GFAPI_RECSZ=64
 #GFAPI_FSYNC_AT_CLOSE (0) - if 1, then issue fsync() call on file before closing
 export GFAPI_FSYNC_AT_CLOSE=0
 
-export PGFAPI_MOUNTPOINT=/mnt
-
 # PGFAPI_PROCESSES - how many processes per client (default: 4)
 export PGFAPI_PROCESSES=4
 
@@ -27,6 +25,13 @@ export PGFAPI_THREADS_PER_PROC=1
 # PGFAPI_FILES - number of files per thread to use (default 10000)
 export PGFAPI_FILES=100
 
+# PGFAPI_FILESIZE - defaults to 4 (KB), number of KB to write or read per file
+export PGFAPI_FILESIZE=4
+
+# PGFAPI_DIRECT - defaults to 0, if 1 use O_DIRECT flag at open time
+#                 force use of O_DIRECT even for sequential reads/writes
+export PGFAPI_DIRECT=0
+
 # PGFAPI_LOAD - what kind of workload - seq-wr, seq-rd, rnd-wr, rnd-rd, unlink, seq-rdwrmix
 #               default is seq-wr. "seq" = "sequential", "rnd" = "random", "wr" = "write", "rd" = "read"
 export PGFAPI_LOAD=seq-wr
@@ -34,12 +39,11 @@ export PGFAPI_LOAD=seq-wr
 # PGFAPI_FUSE - defaults to 0, if 1 use equivalent POSIX fs calls instead of libgfapi
 export PGFAPI_FUSE=1
 
-# PGFAPI_DIRECT - defaults to 0, if 1 use O_DIRECT flag at open time
-#                 force use of O_DIRECT even for sequential reads/writes
-export PGFAPI_DIRECT=0
+# if PGFAPI_FUSE=1, then need to set PGFAPI_MOUNTPOINT
+export PGFAPI_MOUNTPOINT=/mnt
 
-# PGFAPI_FILESIZE - defaults to 4 (KB), number of KB to write or read per file
-export PGFAPI_FILESIZE=4
+# PGFAPI_LOGDIR - output log directory, default value is /tmp
+export PGFAPI_LOGDIR=$CURRENT_PATH/output
 
 
 export PGFAPI_PROGRAM=$CURRENT_PATH/gfapi_perf_test
